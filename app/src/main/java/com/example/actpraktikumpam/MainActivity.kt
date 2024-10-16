54ac72fa365fb4cd4701f3eb9f1f89342a70dd9a
namira121 <127211008+namira121@users.noreply.github.com>
2024-10-17 03:56:32 +0700

    memanggil file foto dari drawable

--- app/src/main/java/com/example/actpraktikumpam/MainActivity.kt
+++ app/src/main/java/com/example/actpraktikumpam/MainActivity.kt
@@ -15,6 +15,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -64,6 +65,9 @@ fun BasicCompose( modifier: Modifier = Modifier){
         Text(text = "Namira Anjani",
             fontWeight = FontWeight.SemiBold,
             fontSize = 25.sp
+        )
+        Image(painter = painterResource(id = R.drawable.foto),
+
         )
     }
 }
