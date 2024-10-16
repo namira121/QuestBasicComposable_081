02439684825e5dc30d312e80765ee833b36a4b3f
namira121 <127211008+namira121@users.noreply.github.com>
2024-10-17 03:35:30 +0700

    membuat fungsi BasicCompose dan Column baru

---
+++ app/src/main/java/com/example/actpraktikumpam/MainActivity.kt
@@ -0,0 +1,79 @@
+package com.example.actpraktikumpam
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.example.actpraktikumpam.ui.theme.ActPraktikumPAMTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ActPraktikumPAMTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    BasicCompose(modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
+@Preview(showBackground = true)
+@Composable
+fun BasicCompose( modifier: Modifier = Modifier){
+
+    Column(horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Text(text = "Indonesia",
+            style = TextStyle(
+                fontSize = 50.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.Blue)
+        )
+        Text(
+            text = "0",
+            fontWeight = FontWeight.Bold,
+            fontSize = 25.sp
+        )
+
+        Image(painter = painterResource(id = R.drawable.logofifa3),
+            contentDescription = null
+        )
+
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ActPraktikumPAMTheme {
+        Greeting("Android")
+    }
+}
